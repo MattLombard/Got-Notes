050413ac7e1a6e4720c19455fd071f5e59611cca
Matt Lombard <ncmattlombard@gmail.com>
2023-04-15 20:00:56 -0400

    created the index js file for the routes folder set up the routes for the notes and index html files

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+const path = require('path'); // require path module
+const router = require('express').Router(); // require express router
+
+// GET route for notes
+router.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/notes.html')); // send notes.html
+});
+
+// GET route for index
+router.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/index.html')); // send index.html
+});
+
+module.exports = router;
